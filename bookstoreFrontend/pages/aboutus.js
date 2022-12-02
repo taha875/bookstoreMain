50f6d638791f766f6dce0cefc98b9fa905299adb
8dar69 <badarsohail02@gmail.com>
2022-12-02 18:42:59 +0500

    badar

--- bookstoreFrontend/pages/aboutus.js
+++ bookstoreFrontend/pages/aboutus.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import Home from "../components/LandingPage/home/home";
 import Footer from "../components/layout/footer";
 import Aboutus from "../components/Aboutus/index";
