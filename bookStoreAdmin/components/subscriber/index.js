8cc26d0f92e65b573ddac62e4d72a6cd9c2f74ae
taha875 <tahaosama25@gmail.com>
2022-12-09 16:52:28 +0500

    homepage

--- bookStoreAdmin/components/subscriber/index.js
+++ bookStoreAdmin/components/subscriber/index.js
@@ -5,9 +5,8 @@ import SubTable from "../Reuseabletable/Table/SubTable";
 function index() {
   return (
     <>
-      <div></div>
       <div className="p-4 md:p-8 xl:p-10 ">
-        <div className="flex flex col items-center mb-2 font-semibold">
+        <div className="flex col items-center mb-2 font-semibold">
           <div className="font-semibold text-2x1 mr-2 ">
             Total Number of Subcriber
           </div>
